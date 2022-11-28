e46aa36982d0445f799b0d2783af0a9332d890d3
Farshid Ashouri <farshid.ashouri@revolut.com>
2022-11-28 21:38:31 +0000

    A simple project structure

---
+++ Makefile
@@ -0,0 +1,4 @@
+typecheck:
+	@mypy -p app
+
+
---
+++ app/sample.py
@@ -0,0 +1,15 @@
+"""
+this file includes required functions for a fucking task
+"""
+import os
+
+
+def get_os_name() -> str:
+    """
+    returns os name by calling os library
+    """
+    return os.name
+
+
+def get_double(number: int) -> int:
+    return number * 2
